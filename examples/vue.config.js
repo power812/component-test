5b160b1271c2d45e98e16b3e69068820c49f5372
power <1410837981@qq.com>
2023-07-27 19:04:35 +0800

    feat: 多组件

---
+++ examples/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  publicPath: './',
+  pages: {
+    index1: {
+      entry: './src/main.js',
+      template: './public/index.html',
+    },
+    index2: {
+      entry: './src/main.js',
+      template: './public/index.html',
+    },
+  }
+};
